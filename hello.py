934d90e3b46104a040862c42bb3aaba91a40c334
Jesslyn Kimiko <jesslynkimiko_2428250005@mhs.mdp.ac.id>
2025-11-18 13:54:24 +0700

    contact

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #import flask module
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 #create an instance of the Flask class
 app = Flask(__name__, template_folder='views')
@@ -12,9 +12,19 @@ def hello_world():
 @app.route('/about')
 def about():
     return render_template('about.html')
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'POST':
+        #proses data form di sini
+        nama = request.form['nama']
+        email = request.form['email']
+        pesan = request.form['pesan']
+
+        #tampilkan pada terminal
+        print(f'Nama: {nama}, Email: {email}, Pesan: {pesan}')
+
+    title = "Contact Page"
+    return render_template('contact.html', title=title)
 
 @app.route('/pmb')
 def pmb():
